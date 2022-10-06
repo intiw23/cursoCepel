055ee6ef4d58b3cdc6548012bf0ddfdf00cc1dfe
intiw23 <intiw23@gmail.com>
2022-10-06 09:54:21 -0300

    Update codepy.py

--- codepy.py
+++ codepy.py
@@ -1,17 +1,28 @@
                     
 # Python 'Hello world' program
+import sys
+
 def main():
-  print("Hello world em Python")
-  print(lepoesia)
+    nome = "poesia.txt"
+    #print("Hello world em Python")
+    lepoesias(nome)
+
 
 def lepoesia():
-  arq = open("poesia.txt")
-  linhas = arq.readlines()
-  for linha in linhas:
-    print(linha)
- 
+    arq = open("poesia.txt")
+    linhas = arq.readlines()
+    for linha in linhas:
+        print(linha)
+
+def lepoesias(nome):
+    with open(nome, 'r', encoding='utf-8') as arq_entrada:
+        # CORPO DO WITH
+        conteudo = arq_entrada.read()
+
+    # continue o programa usando conteudo
+    print(conteudo)
+
 if __name__ == '__main__':
     main()
-
 
                 
